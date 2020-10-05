f49b49719e707cece9f1d92ecc4643e157f854cc
Nischayrohilla <70746797+Nischayrohilla@users.noreply.github.com>
2020-10-05 15:51:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,44 +4,53 @@ for(var i=0;i<document.querySelectorAll(".drum").length;i++)
     {
         var buttonInnerhtml=this.innerHTML;
 
-        switch (buttonInnerhtml) {
-            case "w":
-                var audio=new Audio("tom-1.mp3");
-                       audio.play();
-                break;
+        makeSound(buttonInnerhtml);
+    });
+   
+}
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+});
 
-            case "a":
-               var audio=new Audio("tom-2.mp3");
-                 audio.play();
-             break;
+function makeSound(key)
+{
+    switch (key) {
+        case "w":
+            var audio=new Audio("tom-1.mp3");
+                   audio.play();
+            break;
 
-                    case "s":
-                        var audio=new Audio("tom-3.mp3");
+        case "a":
+           var audio=new Audio("tom-2.mp3");
+             audio.play();
+         break;
+
+                case "s":
+                    var audio=new Audio("tom-3.mp3");
+                    audio.play();
+                    break;
+
+                    case "d":
+                        var audio=new Audio("tom-4.mp3");
                         audio.play();
                         break;
-
-                        case "d":
-                            var audio=new Audio("tom-4.mp3");
+                        case "j":
+                            var audio=new Audio("kick-bass.mp3");
                             audio.play();
                             break;
-                            case "j":
-                                var audio=new Audio("kick-bass.mp3");
+                            case "k":
+                            var audio=new Audio("snare.mp3");
+                            audio.play();
+                            break;
+                            case "l":
+                                var audio=new Audio("crash.mp3");
                                 audio.play();
                                 break;
-                                case "k":
-                                var audio=new Audio("snare.mp3");
-                                audio.play();
-                                break;
-                                case "l":
-                                    var audio=new Audio("crash.mp3");
-                                    audio.play();
-                                    break;
-                            
-        
-            default:
-                console.log(buttonInnerhtml);
+                        
+    
+        default:
+            console.log(buttonInnerhtml);
 
-                break;
-        }
-    });
+            break;
+    }
 }
