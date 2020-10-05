6676716aa02d583e787c8b7fd289f9cb2c409864
Nischayrohilla <70746797+Nischayrohilla@users.noreply.github.com>
2020-10-05 15:58:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 for(var i=0;i<document.querySelectorAll(".drum").length;i++)
 {
-    document.querySelectorAll(".drum")[i].addEventListener("click",function ()
+    document.querySelectorAll(".drum")[i].addEventListener("mouseenter",function ()
     {
         var buttonInnerhtml=this.innerHTML;
 
